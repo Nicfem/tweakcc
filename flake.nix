abca0315e0a7d7ed40772e8482f86c92676fca3f
Nicfem <sosiska2004dota2@gmail.com>
2026-05-28 14:39:44 +0300

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "tweakcc";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { default = self.lib.makePackage pkgs; });
+
+    devShells = forAllSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { default = self.lib.makeDevShell pkgs; });
+
+    lib.makePackage = pkgs: pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "tweakcc";
+    version = "4.0.13";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "Piebald-AI";
+      repo = "tweakcc";
+      rev = "44a7d5535584b3d2a687cb512a2e624ab1ab35e5";
+      hash = "sha256-bD5vBTjYswW5BF6Bdt7SHMkOMrMu4yI9MgcpajkZDcE=";
+    };
+
+    pnpmDeps = pkgs.fetchPnpmDeps {
+      inherit (finalAttrs) pname version src;
+      fetcherVersion = 3;
+      hash = "sha256-yFX7lOaGq9SnxElFntg4hfLifIcwWCsRnRhr9VM/8sA=";
+    };
+
+    nativeBuildInputs = [
+      pkgs.nodejs
+      pkgs.pnpm
+      pkgs.pnpmConfigHook
+      pkgs.makeWrapper
+    ];
+
+    buildPhase = ''
+      runHook preBuild
+      pnpm build
+      runHook postBuild
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p $out/lib/tweakcc
+      cp -r --no-preserve=mode,ownership node_modules $out/lib/tweakcc/node_modules
+      cp -r dist $out/lib/tweakcc/
+      makeWrapper ${pkgs.nodejs}/bin/node $out/bin/tweakcc \
+        --add-flags "$out/lib/tweakcc/dist/index.mjs"
+      runHook postInstall
+    '';
+    });
+
+    lib.makeDevShell = pkgs: pkgs.mkShell {
+      packages = [
+        pkgs.nodejs
+        pkgs.pnpm
+      ];
+      shellHook = ''
+        export TWEAKCC_CC_INSTALLATION_PATH="$PWD/node_modules/.bin/claude"
+        export TWEAKCC_CONFIG_DIR="$PWD"
+      '';
+    };
+  };
+}
+
